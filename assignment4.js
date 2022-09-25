95b78b906bc4a4a680a5a0cfc0ef393d212c2fad
rajithsuvarna <rajithsuvarna49@gmail.com>
2022-09-25 18:17:27 +0530

    assignment submission

---
+++ assignment4.js
@@ -0,0 +1,36 @@
+ratingData = [
+  { restaurant: "KFC", rating: 5 },
+  { restaurant: "Burger King", rating: 4 },
+  { restaurant: "KFC", rating: 3 },
+  { restaurant: "Domino", rating: 2 },
+  { restaurant: "Subway", rating: 3 },
+  { restaurant: "Domino", rating: 1 },
+  { restaurant: "Subway", rating: 4 },
+  { restaurant: "Pizza Hut", rating: 5 },
+];
+
+const headers = Array.from(
+  new Set(ratingData.map(({ restaurant }) => restaurant))
+);
+let arr = [];
+headers.map((head) => {
+  let total = 0;
+  let count = 0;
+  const filteredratingData = ratingData.filter((obj) => obj.restaurant == head);
+  filteredratingData.map((item) => {
+    total = filteredratingData.reduce((prev, next) => prev + next.rating, 0);
+    count++;
+  });
+  arr.push({ restaurant: head, averageRating: (total / count).toFixed(2) });
+});
+
+arr.forEach((obj) => {
+  obj.averageRating = +obj.averageRating;
+});
+console.log(arr);
+
+filteredarr = arr.filter((item) => item.averageRating >= 4);
+filteredarr.forEach((obj) => {
+  obj.averageRating = +obj.averageRating;
+});
+console.log(filteredarr);
